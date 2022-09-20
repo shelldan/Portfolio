f52a47b08f8b23c3fcdff604ccdee805a3ce1d2a
shelldan <shelldanlee@gmail.com>
2022-09-19 22:11:12 -0500

    added-first-project

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var firstProject = document.getElementById('first-project')
+
+function redirectFirstProject (){
+    window.location.assign('https://shelldan.github.io/taste_makers/')
+}
+
+firstProject.addEventListener('click',redirectFirstProject)
