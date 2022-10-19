4b3cfcaaa96a63447ceb7bebd81f121498f6aae9
shelldan <shelldanlee@gmail.com>
2022-10-19 15:13:52 -0500

    update link to taste maker project;

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var firstProject = document.getElementById('first-project')
 
 function redirectFirstProject (){
-    window.location.assign('https://shelldan.github.io/taste_makers/')
+    window.location.assign('https://shelldan.github.io/taste-makers/')
 }
 
 firstProject.addEventListener('click',redirectFirstProject)
